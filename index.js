cfb3710750f301e494546b23558b8c3ea1c815b9
Arton Ramadani <artonramadani25@gmail.com>
2025-07-15 20:31:40 +0200

    /get adedd

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Up --- down");
+});
 app.get("/health", (req, res) => {
-  res.send("Server is healthy");
+    res.send("Server is healthy");
 });
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("UP at 5000")
 })
 
